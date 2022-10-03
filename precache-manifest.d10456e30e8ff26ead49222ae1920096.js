344da60f5aab7b8043ac2678213d2cca1e7a1704
Eduardo Lamarche <lamarcheeduardo@gmail.com>
2022-10-02 22:07:48 -0400

    Updates

---
+++ precache-manifest.d10456e30e8ff26ead49222ae1920096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "386091ea3ce01475563a",
+    "url": "/meme-generator/static/css/main.b39b1120.chunk.css"
+  },
+  {
+    "revision": "386091ea3ce01475563a",
+    "url": "/meme-generator/static/js/main.386091ea.chunk.js"
+  },
+  {
+    "revision": "4d3df24a1e3cafc7988c",
+    "url": "/meme-generator/static/js/1.4d3df24a.chunk.js"
+  },
+  {
+    "revision": "340a140dc48e109e29a4",
+    "url": "/meme-generator/static/js/runtime~main.340a140d.js"
+  },
+  {
+    "revision": "b14786931456f8b0aa8d4e0c0879be54",
+    "url": "/meme-generator/index.html"
+  }
+];
